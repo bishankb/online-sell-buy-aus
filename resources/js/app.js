af3976464c477dcf06a94eacaa69f50793509e03
bishank1993@gmail.com <bishank1993@gmail.com>
2025-09-15 13:12:22 +1000

    Setuping admin lte.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 import './bootstrap';
+import '../../node_modules/admin-lte/dist/css/adminlte.css';
+import '../../node_modules/admin-lte/dist/js/adminlte.js';
+
---
+++ resources/views/layouts/backend.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <!--begin::Head-->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>AdminLTE v4 | Dashboard</title>
+    <!--begin::Accessibility Meta Tags-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+    <meta name="color-scheme" content="light dark" />
+    <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)" />
+    <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)" />
+    <!--end::Accessibility Meta Tags-->
+    <!--begin::Primary Meta Tags-->
+    <meta name="title" content="AdminLTE v4 | Dashboard" />
+    <meta name="author" content="ColorlibHQ" />
+    <meta
+      name="description"
+      content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS. Fully accessible with WCAG 2.1 AA compliance."
+    />
+    <meta
+      name="keywords"
+      content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard, accessible admin panel, WCAG compliant"
+    />
+    <!--end::Primary Meta Tags-->
+    <!--begin::Accessibility Features-->
+    <meta name="supported-color-schemes" content="light dark" />
+
+    <!-- Linking style and js -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @yield('backend-style')
+
+  </head>
+  <!--end::Head-->
+  <body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('backend.partials.navbar')
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('backend.partials.sidebar')
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
+        <!--begin::App Content Header-->
+        @include('backend.partials.page-header')
+        <!--end::App Content Header-->
+        <!--begin::App Content-->
+        <div class="app-content">
+          <!--begin::Container-->
+          @yield('content')
+          <!--end::Container-->
+        </div>
+        <!--end::App Content-->
+      </main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      <footer class="app-footer">
+        <!--begin::Copyright-->
+        <strong>
+          Copyright &copy; {{ date('Y') }}&nbsp;
+          <a href="{{ env('APP_URL')}}" class="text-decoration-none">{{ env('APP_NAME') }}</a>.
+        </strong>
+        All rights reserved.
+        <!--end::Copyright-->
+      </footer>
+      <!--end::Footer-->
+    </div>
+    <!--end::App Wrapper-->
+   
+  </body>
+  <!--end::Body-->
+</html>
