007f2f3a5497423e84335375a94446f74273b716
bishank1993@gmail.com <bishank1993@gmail.com>
2025-09-25 21:47:07 +1000

    Making nav-link active in sidebar.

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -40,15 +40,15 @@
         </li>
 
         @can('view_categories')
-            <li class="nav-item {{Request::is('admin/categories*') ? 'active' : ''}}">
-                <a href="{{route('categories.index')}}" class="nav-link">
-                  <i class="fa fa-list" style="margin: 5px;"></i><span>Category</span>
-                </a>
-            </li>
+          <li class="nav-item">
+              <a href="{{route('categories.index')}}" class="nav-link {{Request::is('admin/categories*') ? 'active' : ''}}">
+                <i class="fa fa-list" style="margin: 5px;"></i><span>Category</span>
+              </a>
+          </li>
         @endcan
         @can('view_sub_categories')
-            <li class="nav-item {{Request::is('admin/sub-categories*') ? 'active' : ''}}">
-                <a href="{{route('sub-categories.index')}}" class="nav-link">
+            <li class="nav-item">
+                <a href="{{route('sub-categories.index')}}" class="nav-link {{Request::is('admin/sub-categories*') ? 'active' : ''}}">
                   <i class="fa fa-list-alt" style="margin: 5px;"></i><span>SubCategory</span>
                 </a>
             </li>
@@ -56,7 +56,7 @@
         
         @can('view_users')
           <li class="nav-item {Request::is('admin/users*') ? 'active' : ''}}">
-            <a href="{{route('users.index')}}" class="nav-link">
+            <a href="{{route('users.index')}}" class="nav-link {{Request::is('admin/users*') ? 'active' : ''}}">
               <i class="fa fa-user" style="margin: 5px;"></i>
               <p>Users</p>
             </a>
@@ -64,37 +64,37 @@
         @endcan
 
         @can('view_dashboards')
-          <li class="nav-item {{Request::is('admin/contact-us*') ? 'active' : ''}}">
-              <a href="{{route('contact-us.edit')}}" class="nav-link">
+          <li class="nav-item">
+              <a href="{{route('contact-us.edit')}}" class="nav-link {{Request::is('admin/contact-us*') ? 'active' : ''}}">
                 <i class="fa fa-phone" style="margin: 5px;"></i><span>Contact Us</span>
               </a>
           </li>
         @endcan
 
         @can('view_faqs')
-            <li class="nav-item {{ Request::is('admin/faqs*') ? 'active' : '' }}">
-                <a href="{{ route('faqs.index') }}" class="nav-link">
+            <li class="nav-item">
+                <a href="{{ route('faqs.index') }}" class="nav-link {{ Request::is('admin/faqs*') ? 'active' : '' }}">
                     <i class="fa fa-question-circle" style="margin: 5px;"></i><span>FAQ</span>
                 </a>
             </li>
         @endcan
         @can('view_cities')
-          <li class="nav-item {{Request::is('admin/cities*') ? 'active' : ''}}">
-              <a href="{{route('cities.index')}}" class="nav-link">
+          <li class="nav-item">
+              <a href="{{route('cities.index')}}" class="nav-link {{Request::is('admin/cities*') ? 'active' : ''}}">
                 <i class="fa fa-map-marker" style="margin: 5px;"></i><span>City</span>
               </a>
           </li>
         @endcan
         @can('view_countries')
-          <li class="nav-item {{Request::is('admin/countries*') ? 'active' : ''}}">
-            <a href="{{route('countries.index')}}" class="nav-link">
+          <li class="nav-item">
+            <a href="{{route('countries.index')}}" class="nav-link {{Request::is('admin/countries*') ? 'active' : ''}}">
               <i class="fa fa-globe" style="margin: 5px;"></i><span>Country</span>
             </a>
           </li>
         @endcan
         @can('view_roles')
-            <li class="nav-item {{ Request::is('admin/roles*') ? 'active' : '' }}">
-                <a href="{{ route('roles.index') }}" class="nav-link">
+            <li class="nav-item ">
+                <a href="{{ route('roles.index') }}" class="nav-link {{ Request::is('admin/roles*') ? 'active' : '' }}">
                     <i class="fa fa-adn" style="margin: 5px;"></i><span>Roles</span>
                 </a>
             </li>
