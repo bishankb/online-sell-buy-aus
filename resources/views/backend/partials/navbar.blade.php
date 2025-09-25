5acc0390a67282c7c0eabf6cac34b12b1141619a
bishank1993@gmail.com <bishank1993@gmail.com>
2025-09-25 23:19:17 +1000

    Fixing design issue with user icon in navbar.

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -52,8 +52,8 @@
       <li class="nav-item dropdown user-menu">
         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
           <img
-            src="./assets/img/user2-160x160.jpg"
-            class="user-image rounded-circle shadow"
+            src="{{ asset('adminlte/img/user2-160x160.jpg') }}"
+            class="user-image rounded-circle shadow d-md-inline"
             alt="User Image"
           />
           <span class="d-none d-md-inline">Alexander Pierce</span>
@@ -62,10 +62,11 @@
           <!--begin::User Image-->
           <li class="user-header text-bg-primary">
             <img
-              src="./assets/img/user2-160x160.jpg"
-              class="rounded-circle shadow"
-              alt="User Image"
-            />
+                src="{{ asset('adminlte/img/user2-160x160.jpg') }}"
+                class="rounded-circle shadow"
+                alt="User Image"
+                style="display:block; margin: 0 auto;"
+              />
             <p>
               Alexander Pierce - Web Developer
               <small>Member since Nov. 2023</small>
