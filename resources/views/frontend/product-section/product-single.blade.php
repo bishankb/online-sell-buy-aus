47f3456e41a01c6df71dd7a0ff0cb4b34ba4a866
bishank1993@gmail.com <bishank1993@gmail.com>
2025-10-01 21:13:52 +1000

    Removing etlarge and adding boostrap 5 carousel to product images.Fixing design issues.

--- resources/views/frontend/product-section/product-single.blade.php
+++ resources/views/frontend/product-section/product-single.blade.php
@@ -6,150 +6,173 @@
 	
 	<div class=" single_top">
 	  	<div class="single_grid">
-			<div class="col-md-4 col-sm-6 col-xs-12 image-slider">
-				@if(count($product->images) > 0)
-					<ul id="etalage">
-						@foreach($product->images as $image)
+	  		<div class="row">
+				<div class="col-md-4 col-sm-6 col-12 image-slider">
+				    <div id="productCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+					    <div class="carousel-inner">
+					        @if($product->images->count() > 0)
+					            @foreach($product->images as $key => $image)
+					                <div class="carousel-item @if($key == 0) active @endif">
+					                    <a data-fancybox="gallery" href="{{ asset('storage/media/product/'.$product->id.'/'.$image->filename) }}">
+					                        <img src="{{ asset('storage/media/product/'.$product->id.'/'.$image->filename) }}"class="d-block w-100" alt="Product Image">
+					                    </a>
+					                </div>
+					            @endforeach
+					        @else
+					            <div class="carousel-item active">
+					                <img src="{{ asset('frontend-template/img/no-image.jpg') }}" class="d-block w-100" alt="No Image">
+					            </div>
+					        @endif
+					    </div>
+
+					    @if($product->images->count() > 1)
+					        <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
+					            <span class="carousel-control-prev-icon" aria-hidden="true" style="color:#000;"></span>
+					            <span class="visually-hidden">Previous</span>
+					        </button>
+					        <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
+					            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					            <span class="visually-hidden">Next</span>
+					        </button>
+					        <br>
+					    @endif
+
+						<!-- Thumbnails -->
+					    @if($product->images->count() > 1)
+						    <div class="carousel-thumbnails d-flex justify-content-center gap-2" style="float: left;">
+						        @foreach($product->images as $key => $image)
+						            <img src="{{ asset('storage/media/product/'.$product->id.'/'.$image->filename) }}" class="img-thumbnail thumbnail-image @if($key == 0) active-thumb @endif" data-bs-target="#productCarousel" data-bs-slide-to="{{ $key }}" alt="Thumbnail" style="cursor:pointer; width: 100px; height: 120px; object-fit: cover;">
+						        @endforeach
+						    </div>
+					    @endif
+					</div>
+				</div>
+
+				<div class="col-md-8 col-sm-6 col-12 product-info">
+					@if($product->is_sold == 1)
+						<div class="product-general-sold">
+							<h3>Sorry!!! This product has already been sold.</h3>
+						</div>
+					@endif
+					<div class="cart-a">
+						<div class="product-name">{{ $product->title }}</div>
+						<h5 class="now-get get-cart-in">
+							<span class="badge bg-danger">Total Views: {{ views($product)->count() }}</span>
+						</h5>
+
+						<div class="clearfix"></div>
+					</div>
+					<div class="cart-b">
+						<div class="left-n ">{{ Number::currency($product->price, 'AUD') }}
+							@if($product->is_negotiable == 1) 
+								<span style="font-size: 18px;">(Negotiable)</span> 
+							@endif
+						</div>
+						<h5 class="now-get get-cart-in">
+							<span class="badge bg-success">{{ \App\Models\Product::ConditionType[$product->condition_type] }}</span>
+						</h5>
+
+						<div class="clearfix"></div>
+					</div>
+					<div class="row product-general">
+						<div class="col-md-6">
+							<div class="product-detail">
+								<h6>General Detail</h6>
+								@isset($product->category)
+									<li>
+										<i class="fa fa-list"></i>
+										<span class="detail-title">Category:</span> {{ $product->category->title }}
+									</li>
+								@endisset
+								@isset($product->subCategory)
+									<li>
+										<i class="fa fa-list-alt"></i>
+										<span class="detail-title">SubCategory:</span> {{ $product->subCategory->title }}
+									</li>
+								@endisset
+								<li>
+									<i class="fa fa-clock-o"></i>
+									<span class="detail-title">Posted On:</span> {{ $product->created_at->format('d M, Y') }}
+								</li>
+								<li>
+									<i class="fa fa-times-circle-o"></i>
+									<span class="detail-title">Expried On:</span>
+									@if($product->expiry_period < Carbon\Carbon::now())
+										<span style="color: red">Expired</span>
+									@else
+										{{ Carbon\Carbon::parse($product->expiry_period)->format('d M, Y') }}
+									@endif  
+								</li>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="product-detail">
+								<h6>Seller Detail</h6>
+								<li>
+									<i class="fa fa-user"></i>										
+									<span class="detail-title">Seller Name: </span> {{ $product->createdBy->name }}
+								</li>
+								@if(isset($product->createdBy->profile->address) || isset($product->createdBy->profile->city) || isset($product->createdBy->profile->country))
+									<li>
+										<i class="fa fa-location-arrow"></i>										
+										<span class="detail-title">Location: </span>{{ $product->createdBy->profile->address }}, {{ $product->createdBy->profile->city->name }}, {{ $product->createdBy->profile->country->name }}
+									</li>
+								@endif
+								<li>
+									<i class="fa fa-envelope"></i>										
+									<span class="detail-title">Email:</span> {{ $product->createdBy->email }}
+								</li>
+								@isset($product->createdBy->profile->phone1)
+									<li>
+										<i class="fa fa-phone"></i>										
+										<span class="detail-title">Phone:</span> {{ $product->createdBy->profile->phone1 }}
+									</li>
+								@endisset
+								@isset($product->createdBy->profile->phone2)
+									<li>
+										<i class="fa fa-phone"></i>										
+										<span class="detail-title">Secondary Phone:</span> {{ $product->createdBy->profile->phone2 }}
+									</li>
+								@endisset
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="product-general-description">
+						<h6>Description:</h6>
+						<p>{!! $product->description !!}</p>
+					</div>
+					<br>
+					
+					<div class="share">
+						<h6>Share Product :</h6>
+						<ul class="share_nav">
 							<li>
-								<a href="/storage/media/product/{{ $product->id }}/{{ $image->filename }}" data-width="2048" data-height="1365">
-									<img class="etalage_thumb_image" src="/storage/media/product/{{ $product->id }}/{{ $image->filename }}" alt=" "/>
-
-									<img class="etalage_source_image" src="/storage/media/product/{{ $product->id }}/{{ $image->filename }}" alt=" "/>
-
+								<a href="https://www.facebook.com/sharer/sharer.php?u=obsnepal.com/view-product/{{$product->slug}}&display=popup" title="Share on Facebook" target="__blank">
+									<img src="{{ asset('frontend-template/img/facebook.png') }}" title="facebook">
 								</a>
 							</li>
-						@endforeach
-					</ul>
-				@else 
-					<img src="{{ asset('frontend-template/img/no-image.jpg') }}" alt=" " class="img-responsive" style="margin-bottom: 55px;" />
-				@endif
-				<div class="clearfix"> </div>		
-			</div> 
-			
-			<div class="col-md-8 col-sm-6 col-xs-12 product-info">
-				@if($product->is_sold == 1)
-					<div class="product-general-sold">
-						<h3>Sorry!!! This product has already been sold.</h3>
-					</div>
-				@endif
-				<div class="cart-a">
-					<div class="product-name">{{ $product->title }}</div>
-					<h5 class="now-get get-cart-in">
-						<span class="badge bg-danger">Total Views: {{ views($product)->count() }}</span>
-					</h5>
-
-					<div class="clearfix"></div>
-				</div>
-				<div class="cart-b">
-					<div class="left-n ">{{ Number::currency($product->price, 'AUD') }}
-						@if($product->is_negotiable == 1) 
-							<span style="font-size: 18px;">(Negotiable)</span> 
-						@endif
-					</div>
-					<h5 class="now-get get-cart-in">
-						<span class="badge bg-success">{{ \App\Models\Product::ConditionType[$product->condition_type] }}</span>
-					</h5>
-
-					<div class="clearfix"></div>
-				</div>
-				<div class="row product-general">
-					<div class="col-md-6">
-						<div class="product-detail">
-							<h6>General Detail</h6>
-							@isset($product->category)
-								<li>
-									<i class="fa fa-list"></i>
-									<span class="detail-title">Category:</span> {{ $product->category->title }}
-								</li>
-							@endisset
-							@isset($product->subCategory)
-								<li>
-									<i class="fa fa-list-alt"></i>
-									<span class="detail-title">SubCategory:</span> {{ $product->subCategory->title }}
-								</li>
-							@endisset
 							<li>
-								<i class="fa fa-clock-o"></i>
-								<span class="detail-title">Posted On:</span> {{ $product->created_at->format('d M, Y') }}
+								<a href="https://twitter.com/intent/tweet?url={{ Request::fullUrl() }}" title="Share on Twitter" target="__blank">
+									<img src="{{ asset('frontend-template/img/twitter.png') }}" title="Twiiter">
+								</a>
 							</li>
 							<li>
-								<i class="fa fa-times-circle-o"></i>
-								<span class="detail-title">Expried On:</span>
-								@if($product->expiry_period < Carbon\Carbon::now())
-									<span style="color: red">Expired</span>
-								@else
-									{{ Carbon\Carbon::parse($product->expiry_period)->format('d M, Y') }}
-								@endif  
+								<a href="https://plus.google.com/share?url={{ Request::fullUrl() }}" title="Share on Google+" target="__blank">
+									<img src="{{ asset('frontend-template/img/gpluse.png') }}" title="Google+">
+								</a>
 							</li>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="product-detail">
-							<h6>Seller Detail</h6>
-							<li>
-								<i class="fa fa-user"></i>										
-								<span class="detail-title">Seller Name: </span> {{ $product->createdBy->name }}
-							</li>
-							@if(isset($product->createdBy->profile->address) || isset($product->createdBy->profile->city) || isset($product->createdBy->profile->country))
-								<li>
-									<i class="fa fa-location-arrow"></i>										
-									<span class="detail-title">Location: </span>{{ $product->createdBy->profile->address }}, {{ $product->createdBy->profile->city->name }}, {{ $product->createdBy->profile->country->name }}
-								</li>
-							@endif
-							<li>
-								<i class="fa fa-envelope"></i>										
-								<span class="detail-title">Email:</span> {{ $product->createdBy->email }}
-							</li>
-							@isset($product->createdBy->profile->phone1)
-								<li>
-									<i class="fa fa-phone"></i>										
-									<span class="detail-title">Phone:</span> {{ $product->createdBy->profile->phone1 }}
-								</li>
-							@endisset
-							@isset($product->createdBy->profile->phone2)
-								<li>
-									<i class="fa fa-phone"></i>										
-									<span class="detail-title">Secondary Phone:</span> {{ $product->createdBy->profile->phone2 }}
-								</li>
-							@endisset
-						</div>
+						</ul>
 					</div>
 				</div>
-				<br>
-				<div class="product-general-description">
-					<h6>Description:</h6>
-					<p>{!! $product->description !!}</p>
-				</div>
-				<br>
-				
-				<div class="share">
-					<h6>Share Product :</h6>
-					<ul class="share_nav">
-						<li>
-							<a href="https://www.facebook.com/sharer/sharer.php?u=obsnepal.com/view-product/{{$product->slug}}&display=popup" title="Share on Facebook" target="__blank">
-								<img src="{{ asset('frontend-template/img/facebook.png') }}" title="facebook">
-							</a>
-						</li>
-						<li>
-							<a href="https://twitter.com/intent/tweet?url={{ Request::fullUrl() }}" title="Share on Twitter" target="__blank">
-								<img src="{{ asset('frontend-template/img/twitter.png') }}" title="Twiiter">
-							</a>
-						</li>
-						<li>
-							<a href="https://plus.google.com/share?url={{ Request::fullUrl() }}" title="Share on Google+" target="__blank">
-								<img src="{{ asset('frontend-template/img/gpluse.png') }}" title="Google+">
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-	  	    <div class="clearfix"> </div>
+		  	    <div class="clearfix"> </div>
+		  	</div>
 	  	</div>
 	  	<br>
 
 	  	<div class="row">
 	      	<div class="col-md-12">
-	          	<div class="card mb-3 card-single">
+	          	<div class="card card-single">
 				    <div class="card-header bg-primary text-white">
 				    	<h5><i class="fa fa-money"></i>Pricing Detail</h5>
 				    </div>
@@ -186,7 +209,7 @@
 		@if($product->has_home_delivery == 1 || isset($product->delivery_area) || isset($product->delivery_charge))
 			<div class="row">
 		      	<div class="col-md-12">
-		          	<div class="card mb-3 card-single">
+		          	<div class="card card-single">
 					    <div class="card-header bg-primary text-white">
 					    	<h5><i class="fa fa-plane"></i>Delivery Detail</h5>
 					    </div>
@@ -236,7 +259,7 @@
 		)
 			<div class="row">
 		      	<div class="col-md-12">
-		          	<div class="card mb-3 card-single">
+		          	<div class="card card-single">
 					    <div class="card-header bg-primary text-white">
 					    	<h5><i class="fa fa-info-circle"></i>Product Detail</h5>
 					    </div>
@@ -334,7 +357,7 @@
 		@isset($product->features)
 			<div class="row">
 		      	<div class="col-md-12">
-		          	<div class="card mb-3 card-single">
+		          	<div class="card card-single">
 					    <div class="card-header bg-primary text-white">
 					    	<h5><i class="fa fa-star"></i>Feature</h5>
 					    </div>
@@ -423,22 +446,24 @@
 
 <script type="text/javascript">
 	document.addEventListener('DOMContentLoaded', function () {
-		$('#etalage').etalage({
-			thumb_image_width: 300,
-			thumb_image_height: 400,
-			source_image_width: 900,
-			source_image_height: 1200,
-			show_hint: false,
-			click_callback: function(image_anchor, instance_id){
-				var index = $( '[href="' + image_anchor + '"]' ).first().parents('li').index();
-				$.fancybox(allimages, { index: index });
-			}
-		});
+		// Collect all source images for Fancybox
+	    var allimages = [];
+	    $('.etalage_source_image').each(function(){
+	        allimages.push({ href: $(this).attr('src') });
+	    });
 
-		var allimages = [];
-		$('.etalage_source_image').each(function(){
-			allimages.push({ href:$(this).attr('src') });
-		});
+	    // Initialize Etalage
+	    $('#etalage').etalage({
+	        thumb_image_width: 300,
+	        thumb_image_height: 400,
+	        source_image_width: 900,
+	        source_image_height: 1200,
+	        show_hint: false,
+	        click_callback: function(image_anchor, instance_id){
+	            var index = $( '[href="' + image_anchor + '"]' ).first().parents('li').index();
+	            $.fancybox.open(allimages, { index: index });
+	        }
+	    });
 
 		$("#relatedProductSlider").flexisel({
 			visibleItems: 4,
