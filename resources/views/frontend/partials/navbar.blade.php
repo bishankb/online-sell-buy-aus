9426a50aa84a692e7fbd0f4a868ca9973c0009e3
bishank1993@gmail.com <bishank1993@gmail.com>
2025-10-06 20:43:28 +1000

    Integrating notification in frontend in mobile screen.

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -33,11 +33,41 @@
 					@if (Auth::user())
 						<ul class="login notify-bell">
 	                        <li class="dropdown notification-btn">
-					      		<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="nav_notification">
+					      		<a class="dropdown-toggle" data-bs-toggle="dropdown" href="#" id="nav_notification">
 	                                <i class="fa fa-bell" aria-hidden="true"></i>
-	              					
+	              					@if(Auth::user()->unreadNotifications->count())
+	                                    <b class="badge badge-custom" style="">{{ Auth::user()->unreadNotifications->count() }}</b>
+	                                @endif
 	                            </a>
-	                            
+	                            <ul class="dropdown-menu notify-drop custom-dropdown">
+	                                <div class="notify-drop-title">
+                                        <strong>Notification(s)</strong>
+                                        @if(Auth::user()->unreadNotifications->count() >0)
+                                            <a style="float: right;" href="{{ route('notification.mark-read') }}"title="Mark All Notification as Read">
+							                    <span class="badge bg-danger">Clear All</span>
+							                </a>
+                                        @endif
+	                                </div>
+	                                <div class="drop-content">
+	                                    @foreach(Auth::user()->unreadNotifications->take(10) as $unreadNotification) 
+	                                        @if(isset($unreadNotification->data['message']))
+							                    <li>
+                                					@if(isset($unreadNotification->data['url']))
+							                      	  <a style="padding: 0 !important;" href="{{ url($unreadNotification->data['url']). '?' . http_build_query(['notify_id' => $unreadNotification->id])}}">
+							                        		<p>{{ $unreadNotification->data['message'] }}</p
+							                        			>
+							                        	</a>
+							                        @endif
+							                    </li>
+							               	@endif
+	                                    @endforeach
+	                                </div>
+	                                <div class="notify-drop-footer text-center" style="clear: both;">
+	                                    <a href="{{ route('notification.view-notification') }}">
+	                                        <i class="fa fa-eye"></i> See All
+	                                    </a>
+	                                </div>
+	                            </ul>
 	                        </li>
 						</ul>
 						<ul class="login">
@@ -95,25 +125,40 @@
 			<!--Large Screen-->
 			<div class="login-section">					
 				@if (Auth::user())
-					<ul class="login" style="width: 30px;">
+					<ul class="login">
                         <li class="dropdown notification-btn">
-				      		<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="nav_notification">
+				      		<a class="dropdown-toggle" data-bs-toggle="dropdown" href="#" id="nav_notification">
                                 <i class="fa fa-bell" aria-hidden="true"></i>
-              					
+              					@if(Auth::user()->unreadNotifications->count())
+                                    <b class="badge badge-custom" style="">{{ Auth::user()->unreadNotifications->count() }}</b>
+                                @endif
                             </a>
                             <ul class="dropdown-menu notify-drop custom-dropdown">
-                                <div class="notify-drop-title">
-                                    <div class="row">
-                                        <div class="col-md-6 col-sm-6 col-xs-6"><strong>Notification(s)</strong></div>
-                                        
-                                    </div>
+                                <div class="notify-drop-title text-center">
+                                        <strong>Notification(s)</strong>
+                                        @if(Auth::user()->unreadNotifications->count() >0)
+                                                <a href="{{ route('notification.mark-read') }}"title="Mark All Notification as Read">
+								                    <span class="badge bg-danger">Clear All</span>
+								                </a>
+                                        @endif
                                 </div>
                                 <div class="drop-content">
-                                    
+                                    @foreach(Auth::user()->unreadNotifications->take(10) as $unreadNotification) 
+                                        @if(isset($unreadNotification->data['message']))
+						                    <li>
+                            					@if(isset($unreadNotification->data['url']))
+						                        	<a style="padding: 0 !important;" href="{{ url($unreadNotification->data['url']). '?' . http_build_query(['notify_id' => $unreadNotification->id])}}">
+						                        		<p>{{ $unreadNotification->data['message'] }}</p>
+
+						                        	</a>                    	
+						                        @endif
+						                    </li>
+						               	@endif
+                                    @endforeach
                                 </div>
                                 <div class="notify-drop-footer text-center" style="clear: both;">
 		                            <a href="#">
-                                        <i class="fa fa-eye"></i> See All
+                                        <i class="fa fa-eye"></i> See All Notification
                                     </a>
                                 </div>
                             </ul>
