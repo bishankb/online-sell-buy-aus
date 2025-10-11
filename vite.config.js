4e7224120c4274a12be8113743037c74859b0e39
bishank1993@gmail.com <bishank1993@gmail.com>
2025-10-11 15:16:41 +1000

    Fixing small issue.

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/frontend-app.js', // your main JS file
+                'resources/js/app.js',
+                'resources/js/frontend-app.js',
             ],
-            refresh: true, // live reload in dev
+            refresh: true,
         }),
     ],
     build: {
-        outDir: 'public/build',
         manifest: true,
-        rollupOptions: {
-            input: {
-                frontend: path.resolve(__dirname, 'resources/js/frontend-app.js'),
-            },
-            output: {
-                entryFileNames: '[name].js',      // ensure it stays frontend-app.js
-                chunkFileNames: '[name].js',
-                assetFileNames: '[name].[ext]',
-            },
-        },
-    },
-    server: {
-        host: true,
-        cors: true,
-        hmr: {
-            protocol: 'ws',
-            host: '127.0.0.1',
-        },
+        outDir: 'public/build',
+        emptyOutDir: true,
     },
 });
